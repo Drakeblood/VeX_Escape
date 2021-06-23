3b6279272682e5d6529edf62ec37600f95566750
KhutasLee <56769943+KhutasLee@users.noreply.github.com>
2021-06-23 10:34:55 +0200

    Object pooling init

--- VeX_Escape/Source/VeX_Escape/Private/Components/VEXObjectPoolingComponent.cpp
+++ VeX_Escape/Source/VeX_Escape/Private/Components/VEXObjectPoolingComponent.cpp
@@ -3,32 +3,12 @@
 
 #include "Components/VEXObjectPoolingComponent.h"
 
-// Sets default values for this component's properties
 UVEXObjectPoolingComponent::UVEXObjectPoolingComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
-	// ...
+	for (uint8 i = (uint8)EVEXItemType::Coin; i != (uint8)EVEXItemType::END; i++)
+	{
+		ObjectPools.Add((EVEXItemType)i, NewObject<UObjectPool>());
+	}
 }
-
-
-// Called when the game starts
-void UVEXObjectPoolingComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// ...
-	
-}
-
-
-// Called every frame
-void UVEXObjectPoolingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-}
-
